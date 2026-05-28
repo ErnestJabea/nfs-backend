6c902b0313647586e63797f7557314f0b6dd87e0
ejabbing <ernestjabea@gmail.com>
2026-05-28 22:47:09 +0100

    fix: resolveJsonModule to bundle swagger.json in build output

--- src/utils/swaggerConfig.ts
+++ src/utils/swaggerConfig.ts
@@ -1,13 +1,8 @@
 import swaggerUi from 'swagger-ui-express';
 import { Express } from 'express';
-import * as fs from 'fs';
-import * as path from 'path';
+import swaggerDocument from './swagger.json';
 
 export const setupSwagger = (app: Express) => {
-  const swaggerFile = path.resolve(__dirname, './swagger.json');
-  const swaggerData = fs.readFileSync(swaggerFile, 'utf8');
-  const swaggerDocument = JSON.parse(swaggerData);
-  
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
     explorer: true
   }));
